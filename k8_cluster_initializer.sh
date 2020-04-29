c2a292ab43fe2e0d66d288e560ebff27bc2cd040
David Simowitz <david.simowitz@gmail.com>
2020-04-29 04:45:13 +0000

    feat: create an Amazon EKS cluster

---
+++ k8_cluster_initializer.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+eksctl create cluster \
+ --name cloud-devops-capstone-cluster \
+ --region us-east-1 \
+ --version 1.15 \
+ --nodegroup-name cloud-devops-nodes \
+ --node-ami auto \
+ --node-type t2.medium \
+ --nodes 2 \
+ --nodes-min 1 \
+ --nodes-max 2
