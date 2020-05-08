b352a4e8343fe760fd03582a8b55f32ddb36581d
David Simowitz <david.simowitz@gmail.com>
2020-05-07 20:36:02 -0400

    chore: pull docker image

--- scripts/get-docker-image.sh
+++ scripts/get-docker-image.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-# Pull image from docker repository
-docker login --username DOCKER_CREDS_USR --password DOCKER_CREDS_PSW
+# Pull image from docker registry
+docker login --username $DOCKER_CREDS_USR --password $DOCKER_CREDS_PSW
 docker pull $DOCKER_REPO:$TAG
