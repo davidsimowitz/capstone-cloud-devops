ea72a4cdb60d85926442655458e3505e37c0ed9d
David Simowitz <david.simowitz@gmail.com>
2020-05-07 20:52:10 -0400

    chore: avoid zone us-east-1e when creating cluster

--- scripts/k8-create-cluster.sh
+++ scripts/k8-create-cluster.sh
@@ -3,6 +3,7 @@
 eksctl create cluster \
  --name $CLUSTER \
  --region $REGION \
+ --zones us-east-1a us-east-1b us-east-1c us-east-1d \
  --version 1.15 \
  --nodegroup-name workers \
  --node-ami auto \
